5139598f16b23eeb4888837200cb0c3f2ee86515
Daniel Lechok <dclechok@gmail.com>
2019-09-09 18:23:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if(phrase == phrase.upcase)
     return "NO, NOT SINCE 1938!"
     binding.pry
-  elsif(!(phrase.upcase))
+  elsif(!(phrase == phrase.upcase))
     return "HUH?! SPEAK UP, SONNY!"
   elsif(phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
